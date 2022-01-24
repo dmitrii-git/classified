ff65963cd57fdab4859d6e499f86d237a53b4dda
Dmitrii <mikheevda1@gmail.com>
2022-01-24 13:08:19 -0400

    Initial commit

---
+++ src/main/java/com/mcompany/classified/ClassifiedApplication.java
@@ -0,0 +1,13 @@
+package com.mcompany.classified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassifiedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcompany/classified/ClassifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcompany.classified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
